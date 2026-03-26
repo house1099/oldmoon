94b6675af175a7be446dea676bb86462db10bcbb
house1099 <house1099@gmail.com>
2026-03-27 00:39:12 +0800

    feat: 更新

--- src/lib/constants/master-avatar-frame.ts
+++ src/lib/constants/master-avatar-frame.ts
@@ -2,4 +2,4 @@
  * `thunder-frame.png` 透明內圈約為素材寬高的 ~70%；143% 讓內圈貼齊頭像外徑（裝飾在頭像外、不壓臉）。
  * 若更換框圖，請依新素材微調此值。
  */
-export const MASTER_AVATAR_FRAME_OVERLAY_PERCENT = 143;
+export const MASTER_AVATAR_FRAME_OVERLAY_PERCENT = 150;
