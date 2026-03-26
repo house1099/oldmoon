b15bf3689a28bc6aa0eabffc05d47e2541912418
house1099 <house1099@gmail.com>
2026-03-27 00:54:43 +0800

    feat: 更新

--- src/lib/constants/master-avatar-frame.ts
+++ src/lib/constants/master-avatar-frame.ts
@@ -2,7 +2,7 @@
  * 兩層皆以「頭像容器邊長」為 100%：數值須 **> 100** 讓裝飾畫在頭像外，不壓臉。
  * PNG 內圈約占素材 ~70%，框百分比需依美術微調。
  */
-export const MASTER_AVATAR_FRAME_OVERLAY_PERCENT = 165;
+export const MASTER_AVATAR_FRAME_OVERLAY_PERCENT = 180;
 
 /** 閃電 Lottie；建議 ≥ 框比例，閃電略在雷框外緣 */
 export const MASTER_AVATAR_LIGHTNING_OVERLAY_PERCENT = 178;
